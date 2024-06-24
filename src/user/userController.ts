a8f7918a086f9c13ff451b679051bc10c17f187e
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2024-06-24 20:39:14 +0800

    add userModel

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import createHttpError from 'http-errors'
+import userModel from './userModel'
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // steps to write controller
@@ -9,9 +10,18 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, 'All fields are required')
     return next(error)
   }
+  // check if user already exists
+  const user = await userModel.findOne({ email })
+  if (user) {
+    const error = HttpError(400, 'User already exists')
+    return next(error)
+  }
   // 2. Logic
   // 3. Response
   res.json({ message: 'User registered' })
 }
 
 export { createUser }
+function HttpError(arg0: number, arg1: string) {
+  throw new Error('Function not implemented.')
+}
---
+++ src/user/userModel.ts
@@ -0,0 +1,28 @@
+import mongoose, { mongo } from 'mongoose'
+import { User } from './userTypes'
+
+const userSchema = new mongoose.Schema<User>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+
+export default mongoose.model<User>('User', userSchema)
