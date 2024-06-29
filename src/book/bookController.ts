e7bb00db7cdc76f73a14d26725102eb1174ad81e
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2024-06-29 16:39:11 +0800

    get the url of cloudinary uploaded files

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,47 @@
 import { NextFunction, Request, Response } from 'express'
+import path from 'node:path'
+import cloudinary from '../config/cloudinary'
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: 'Create book' })
+  // console.log(req.files)
+
+  // create a multer type in typescript
+  const files = req.files as { [fieldname: string]: Express.Multer.File[] }
+  // upload cover images to cloudinary
+  const coverImageType = files.coverImage[0].mimetype.split('/')[-1]
+  const fileName = files.coverImage[0].filename
+  const filePath = path.resolve(
+    __dirname,
+    '../../public/data/uploads',
+    fileName
+  )
+
+  const uploadResult = await cloudinary.uploader.upload(filePath, {
+    folder: 'book-covers',
+    filename_override: fileName,
+    format: coverImageType,
+  })
+
+  console.log('Upload result: ', uploadResult)
+
+  // upload pdf files to cloudinary
+  const bookFileName = files.file[0].filename
+  const bookFilePath = path.resolve(
+    __dirname,
+    '../../public/data/uploads',
+    bookFileName
+  )
+
+  const bookUploadResult = await cloudinary.uploader.upload(bookFilePath, {
+    folder: 'books',
+    resource_type: 'raw',
+    filename_override: bookFileName,
+    format: 'pdf',
+  })
+
+  console.log('Book upload result: ', bookUploadResult)
+
+  res.json({ message: 'Book created successfully' })
 }
 
 export { createBook }
--- src/config/config.ts
+++ src/config/config.ts
@@ -1,12 +1,15 @@
-import {config as dotenvConfig} from 'dotenv'
+import { config as dotenvConfig } from 'dotenv'
 dotenvConfig()
 
 // config is already defined in the global scope, so we can't use it as a variable name
 const _config = {
-    port: process.env.PORT || 3000,
-    databaseUrl: process.env.MONGO_URI,
-    env: process.env.NODE_ENV, // To Identify 'development' or 'production'
-    jwtSecret: process.env.JWT_SECRET,
+  port: process.env.PORT || 3000,
+  databaseUrl: process.env.MONGO_URI,
+  env: process.env.NODE_ENV, // To Identify 'development' or 'production'
+  jwtSecret: process.env.JWT_SECRET,
+  cloudinaryName: process.env.CLOUDINARY_CLOUD_NAME,
+  cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+  cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
 }
 
 export const config = Object.freeze(_config) // freeze the object to prevent modification
