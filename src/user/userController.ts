9493570c23d6f5f333c020c4cc682166b187f486
ahmedashfaq850 <ahmedashfaq6777@gmail.com>
2024-06-29 10:33:09 +0800

    add error handling in userCOntroller

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from './userModel'
 import bcrypt from 'bcrypt'
 import { sign } from 'jsonwebtoken'
 import { config } from '../config/config'
+import { User } from './userTypes'
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // steps to write controller
@@ -16,30 +17,46 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
 
   // 2. Logic
   // check if user already exists
-  const user = await userModel.findOne({ email })
-  if (user) {
-    const error = HttpError(400, 'User already exists')
-    return next(error)
+  try {
+    const user = await userModel.findOne({ email })
+    if (user) {
+      const error = createHttpError(400, 'User already exists')
+      return next(error)
+    }
+  } catch (error) {
+    return next(createHttpError(500, 'Something went wrong while getting user'))
   }
 
-  // Hash password
-  const hashPassword = await bcrypt.hash(password, 10)
+  let newUser: User
 
-  // create user
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashPassword,
-  })
+  try {
+    // Hash password
+    const hashPassword = await bcrypt.hash(password, 10)
 
-  // JWT Token
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {expiresIn: '7d'})
+    // create user
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashPassword,
+    })
+  } catch (error) {
+    return next(
+      createHttpError(500, 'Something went wrong while registering user')
+    )
+  }
 
-  //Response
-  res.json({ accessToken: token })
+  try {
+    // JWT Token
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: '7d',
+    })
+    //Response
+    res.json({ accessToken: token })
+  } catch (error) {
+    return next(
+      createHttpError(500, 'Something went wrong while generating token')
+    )
+  }
 }
 
 export { createUser }
-function HttpError(arg0: number, arg1: string) {
-  throw new Error('Function not implemented.')
-}
